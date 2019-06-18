1c2e09d2bbbbf5f505c7ac320a10c94a5e20d141
mwang <mengchen.wang@u-psud.fr>
2019-06-18 16:59:31 +0200

    iCFDriver.h

--- iCFDriver/iCFDriver/iCFDriver.h
+++ iCFDriver/iCFDriver/iCFDriver.h
@@ -2,6 +2,10 @@
 #include "CFDriver.h"
 extern "C"
 {
+#ifdef _WIN64
+
+
+
 	__declspec(dllexport) void InitServer(int port);
 	__declspec(dllexport) void InitClient(const char* adress, int port);
 	__declspec(dllexport) void Disconnect();
@@ -20,5 +24,26 @@ extern "C"
 	__declspec(dllexport) void RecvFile();
 	__declspec(dllexport) void SendData(int len, float* data);	//Backup function
 	__declspec(dllexport) void RecvData(int len, float* data);	//Backup function
+#endif
+
+#ifdef linux
+	extern void InitServer(int port);
+	extern void InitClient(const char* adress, int port);
+	extern void RecvCommand(char & command);
+	extern void InitSimulation();
+	extern void StartSimulation();
+	extern void StopSimulation();
+	extern void ContinueSimulation();
+	extern void SendGeometry(int len, float* geometry);
+	extern void RecvGeometry(int len, float* geometry);
+	extern void SendFlag(int len, int* flag);
+	extern void RecvFlag(int len, int* flag);
+	extern void SendVof(int len, float* vof);
+	extern void RecvVof(int len, float* vof);
+	extern void SendFile(const char* filename);
+	extern void RecvFile();
+	extern void SendData(int len, float* data);	//Backup function
+	extern void RecvData(int len, float* data);	//Backup function
+#endif
 
 }
